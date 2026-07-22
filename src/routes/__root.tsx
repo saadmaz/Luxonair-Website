7d812f2f8d3dd02ab166de9e9705d4f4382a53f1
Saad Mazhar <92235123+saadmazhar03@users.noreply.github.com>
2026-07-22 21:36:37 +0530

    feat: add Google Analytics script for tracking user interactions

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -103,6 +103,16 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     scripts: [
       {
+        src: "https://www.googletagmanager.com/gtag/js?id=G-W2KM9SWBE8",
+        async: true,
+      },
+      {
+        children: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-W2KM9SWBE8');`,
+      },
+      {
         type: "application/ld+json",
         children: JSON.stringify({
           "@context": "https://schema.org",
